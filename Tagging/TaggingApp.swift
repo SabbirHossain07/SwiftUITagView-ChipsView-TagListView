af78b78fae7ef83e65ef76817037a60f37741d5a
SabbirHossain07 <sabbirsopnil07@gmail.com>
2021-10-14 19:25:49 +0600

    Initial Commit

---
+++ Tagging/TaggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaggingApp.swift
+//  Tagging
+//
+//  Created by Sopnil Sohan on 14/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TaggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
